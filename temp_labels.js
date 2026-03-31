17697d6fa7618ab0e40ee0ee5ab7f56f7da4bc50
Sagnik Sen <SagnikSen28@users.noreply.huggingface.co>
2026-03-31 07:25:20 +0000

    Update temp_labels.js

--- temp_labels.js
+++ temp_labels.js
@@ -1,1 +1,3 @@
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png').addTo(map)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
+}).addTo(map);
